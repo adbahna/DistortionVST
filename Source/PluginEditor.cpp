b31a24c62b96e6af728adf29c5f9dee869bf3df2
Alex Bahna <alexbahna@gmail.com>
2019-02-27 15:32:08 -0500

    Subclassed LookAndFeel_V4 (JUCE) to create custom font and slider design

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -18,28 +18,36 @@ DistortionVstAudioProcessorEditor::DistortionVstAudioProcessorEditor (Distortion
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
 
+	LookAndFeel_V4::setDefaultLookAndFeel(&customSlider);
+
 	addAndMakeVisible(driveKnob = new Slider("Drive"));
-	driveKnob->setSliderStyle(Slider::RotaryHorizontalDrag); 
+	driveKnob->setSliderStyle(Slider::RotaryVerticalDrag);
 	driveKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+	driveKnob->setMouseDragSensitivity(150);
 
-	addAndMakeVisible(rangeKnob = new Slider("Range"));
-	rangeKnob->setSliderStyle(Slider::RotaryHorizontalDrag);
+	addAndMakeVisible(rangeKnob = new Slider("Drive"));
+	rangeKnob->setSliderStyle(Slider::RotaryVerticalDrag);
 	rangeKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+	rangeKnob->setMouseDragSensitivity(150);
 
+	 
 	addAndMakeVisible(blendKnob = new Slider("Blend"));
-	blendKnob->setSliderStyle(Slider::RotaryHorizontalDrag);
+	blendKnob->setSliderStyle(Slider::RotaryVerticalDrag);
 	blendKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+	blendKnob->setMouseDragSensitivity(150);
+
 
 	addAndMakeVisible(volumeKnob = new Slider("Volume"));
-	volumeKnob->setSliderStyle(Slider::RotaryHorizontalDrag);
+	volumeKnob->setSliderStyle(Slider::RotaryVerticalDrag);
 	volumeKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+	volumeKnob->setMouseDragSensitivity(150);
 
 	driveAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(), "drive", *driveKnob);
 	rangeAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(), "range", *rangeKnob);
 	blendAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(), "blend", *blendKnob);
 	volumeAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(), "volume", *volumeKnob);
 
-	setSize(425, 200);
+	setSize(400,  150);
 }
 
 DistortionVstAudioProcessorEditor::~DistortionVstAudioProcessorEditor()
@@ -47,18 +55,52 @@ DistortionVstAudioProcessorEditor::~DistortionVstAudioProcessorEditor()
 }
 
 //==============================================================================
-void DistortionVstAudioProcessorEditor::paint (Graphics& g)
+void DistortionVstAudioProcessorEditor::paint(Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+	// (Our component is opaque, so we must completely fill the background with a solid colour)
 
-	g.setColour(Colours::ghostwhite);
+	LookAndFeel_V4::setDefaultLookAndFeel(&customSlider);
+
 	g.setFont(18.0f);
 
-	g.drawText("Drive", ((getWidth() / 5) * 1) - 75, (getHeight() / 2) + 5, 100, 100, Justification::centred, false);
-	g.drawText("Range", ((getWidth() / 5) * 2) - 75, (getHeight() / 2) + 5, 100, 100, Justification::centred, false);
-	g.drawText("Blend", ((getWidth() / 5) * 3) - 75, (getHeight() / 2) + 5, 100, 100, Justification::centred, false);
-	g.drawText("Volume", ((getWidth() / 5) * 4) - 75, (getHeight() / 2) + 5, 100, 100, Justification::centred, false);
+	g.setColour(Colour(37, 36, 34));
+	g.fillRect(0, 0, 400, 150);
+
+	g.setColour(Colour((uint8)0, 0, 0, (uint8)90));
+
+	g.setColour(Colour((uint8)255, 255, 255, (uint8)10));
+	g.fillRect(((getWidth() / 5) * 1) - 57, (getHeight() / 2) + 35, 70, 30);
+	g.fillRect(((getWidth() / 5) * 2) - 40, (getHeight() / 2) + 35, 70, 30);
+	g.fillRect(((getWidth() / 5) * 3) - 26, (getHeight() / 2) + 35, 70, 30);
+	g.fillRect(((getWidth() / 5) * 4) - 11, (getHeight() / 2) + 35, 70, 30);
+
+	g.setColour(Colour((uint8)0, 0, 0, (uint8)10));
+	g.fillRect(((getWidth() / 5) * 1) - 55, (getHeight() / 2) + 36, 70, 30);
+	g.fillRect(((getWidth() / 5) * 2) - 38, (getHeight() / 2) + 36, 70, 30);
+	g.fillRect(((getWidth() / 5) * 3) - 24, (getHeight() / 2) + 36, 70, 30);
+	g.fillRect(((getWidth() / 5) * 4) - 9, (getHeight() / 2) + 36, 70, 30);
+
+
+	g.setColour(Colour((uint8)0, 0, 0, (uint8)90));
+
+	g.drawText("Drive", ((getWidth() / 5) * 1) - 70, (getHeight() / 2) + 2, 100, 100, Justification::centred, false);
+	g.drawText("Range", ((getWidth() / 5) * 2) - 53, (getHeight() / 2) + 2, 100, 100, Justification::centred, false);
+	g.drawText("Blend", ((getWidth() / 5) * 3) - 39, (getHeight() / 2) + 2, 100, 100, Justification::centred, false);
+	g.drawText("Volume", ((getWidth() / 5) * 4) - 24, (getHeight() / 2) + 2, 100, 100, Justification::centred, false);
+
+	g.setColour(Colour(200, 197, 187));
+
+	g.drawText("Drive", ((getWidth() / 5) * 1) - 72, (getHeight() / 2), 100, 100, Justification::centred, false);
+	g.drawText("Range", ((getWidth() / 5) * 2) - 55, (getHeight() / 2), 100, 100, Justification::centred, false);
+	g.drawText("Blend", ((getWidth() / 5) * 3) - 41, (getHeight() / 2), 100, 100, Justification::centred, false);
+	g.drawText("Volume", ((getWidth() / 5) * 4) - 26, (getHeight() / 2), 100, 100, Justification::centred, false);
+
+	g.setColour(Colour((uint8) 255, 255, 255, (uint8)10));
+
+	g.drawText("Drive", ((getWidth() / 5) * 1) - 74, (getHeight() / 2) - 1, 100, 100, Justification::centred, false);
+	g.drawText("Range", ((getWidth() / 5) * 2) - 57, (getHeight() / 2) - 1, 100, 100, Justification::centred, false);
+	g.drawText("Blend", ((getWidth() / 5) * 3) - 43, (getHeight() / 2) - 1, 100, 100, Justification::centred, false);
+	g.drawText("Volume", ((getWidth() / 5) * 4) - 28, (getHeight() / 2) - 1, 100, 100, Justification::centred, false);
 
 }
 
@@ -67,8 +109,8 @@ void DistortionVstAudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
 
-	driveKnob->setBounds(((getWidth() / 5) * 1) - (100 / 2), (getHeight() / 2) - (100 / 2), 100, 100);
-	rangeKnob->setBounds(((getWidth() / 5) * 2) - (100 / 2), (getHeight() / 2) - (100 / 2), 100, 100);
-	blendKnob->setBounds(((getWidth() / 5) * 3) - (100 / 2), (getHeight() / 2) - (100 / 2), 100, 100);
-	volumeKnob->setBounds(((getWidth() / 5) * 4) - (100 / 2), (getHeight() / 2) - (100 / 2), 100, 100);
+	driveKnob->setBounds(((getWidth() / 5) * 1) - 72, (getHeight() / 2) - 65, 100, 100);
+	rangeKnob->setBounds(((getWidth() / 5) * 2) - 57, (getHeight() / 2) - 65, 100, 100);
+	blendKnob->setBounds(((getWidth() / 5) * 3) - 42, (getHeight() / 2) - 65, 100, 100);
+	volumeKnob->setBounds(((getWidth() / 5) * 4) - 27, (getHeight() / 2) - 65, 100, 100);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -12,10 +12,66 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "PluginProcessor.h"
+#include "MainComponent.h"
 
 //==============================================================================
 /**
 */
+
+class CustomSlider : public LookAndFeel_V4 {
+public:
+	CustomSlider()
+	{
+		setColour(Slider::thumbColourId, Colours::red);
+		setDefaultSansSerifTypefaceName("Roboto");
+	}
+	void drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPos,
+		const float rotaryStartAngle, const float rotaryEndAngle, Slider& slider) override
+	{
+		auto radius = jmin(width / 2, height / 2) - 8.0f;
+		auto centreX = x + width * 0.5f;
+		auto centreY = y + height * 0.5f;
+		auto rx = centreX - radius;
+		auto ry = centreY - radius;
+		auto rw = radius * 2.0f;
+		auto angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+
+		// fill
+		g.setColour(Colour((uint8)255, 255, 255, (uint8)64));
+		g.fillEllipse(rx - 1, ry - 2, rw + 1, rw);
+		g.setColour(Colour((uint8)8, 9, 10, (uint8) 64));
+		g.fillEllipse(rx - 2, ry + 4, rw + 4, rw);
+		g.setColour(Colour(64, 61, 57));
+		g.fillEllipse(rx, ry, rw, rw);
+	
+		// outline
+		g.setColour(Colour(68, 65, 60));
+		g.drawEllipse(rx, ry, rw, rw, 2.0f);
+
+		Path p;
+		auto pointerLength = radius * 0.66f;
+		auto pointerThickness = 3.0f;
+		p.addRectangle(-pointerThickness * 0.5f, -radius, pointerThickness, pointerLength);
+		p.applyTransform(AffineTransform::rotation(angle).translated(centreX, centreY));
+
+		// pointer
+		g.setColour(Colour(235, 94, 40));
+		g.fillPath(p);
+	}
+
+	static const Font & getManropeFont()
+	{
+		static Typeface::Ptr manrope = Typeface::createSystemTypefaceFor(BinaryData::bold_otf,
+																	      BinaryData::bold_otfSize);
+
+		return manrope;
+	}
+};
+
+//Add extra look and feel class for image knob testing.
+//Load from cache 
+//Learn about file structure for Juce
+
 class DistortionVstAudioProcessorEditor  : public AudioProcessorEditor
 {
 public:
@@ -27,6 +83,8 @@ public:
     void resized() override;
 
 private:
+
+	CustomSlider customSlider;
 
 	ScopedPointer<Slider> driveKnob;
 	ScopedPointer<Slider> rangeKnob;
